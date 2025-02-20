9bdf32e2a1972dc77136382410359eb47e5eb96c
adityasriv2317 <adityaxia9237@gmail.com>
2025-02-20 15:07:04 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white'>
+      hello
+      <div className="bg-red-500 texl-4xl">hii</div>
+    </div>
+  )
+}
+
+export default App
